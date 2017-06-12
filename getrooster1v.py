af612237c307c1a37d1fe280f04f8023baa0c3df
Timz99 <TimZ99@users.noreply.github.com>
2017-06-12 17:36:17 +0200

    Add getrooster for all VIA classes

---
+++ getrooster1v.py
@@ -0,0 +1,12 @@
+import requests
+import re
+page = requests.get("http://hageveld.mwp.nl/rooster/dagrooster/Ver_Kla.htm").text
+#ro = re.findall(">6b<", page)
+ro = page.find(">1v<")
+if ro == -1:
+    roosterwijziging = "false"
+else:
+    roosterwijziging = "true"
+print 'Klas 1v: ' + roosterwijziging
+print ro
+#print page.find(">5v<")
